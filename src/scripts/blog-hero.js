da0b5db4abee7b04ef5842178805b1f9522567f0
Nelson <nelsonr462@gmail.com>
2018-03-05 18:47:39 -0800

    blog-hero v1, minor fixes/optimizations in homepage-hero, video section, features-large, spectre buttons

---
+++ src/scripts/blog-hero.js
@@ -0,0 +1,16 @@
+var slider = tns({
+    container: '.carousel',
+    nav: true,
+    controls: false,
+    arrowKeys: true,
+    navContainer: '.carousel-nav',
+    items: 1,
+    slideBy: 1,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayButton: false,
+    autoplayButtonOutput: false,
+    lazyload: true,
+    gutter: 10,
+    mouseDrag: true
+  });

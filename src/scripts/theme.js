13dd3916a62c0002f6da5503a9da511926845787
Nelson <nelsonr462@gmail.com>
2019-11-27 22:58:03 -0800

    bfcm theme updates

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -93,4 +93,48 @@ $(document).ready(function() {
     moneyFormat: theme.strings.shippingCalcMoneyFormat
   });
 
+  // Site-wide Bundle Quick-Checkout
+  $(".gqc").click(function (e) { 
+    e.preventDefault();
+    $(".gqc button").attr("disabled", true);
+    addBundle($(".gqc").attr("bundle"));
+});
+
+function addBundle(bundleItems) {
+    bundleItems = bundleItems.split(',');
+    Shopify.queue = [];
+    for (var i = 0; i < bundleItems.length; i++) {
+        Shopify
+            .queue
+            .push({variantId: bundleItems[i]});
+    }
+    Shopify.moveAlong = function () {
+        // If we still have requests in the queue, let's process the next one.
+        if (Shopify.queue.length) {
+            var request = Shopify.queue.shift();
+            var data = 'id=' + request.variantId + '&quantity=1'
+            $.ajax({
+                type: 'POST',
+                url: '/cart/add.js',
+                dataType: 'json',
+                data: data,
+                success: function (res) {
+                    Shopify.moveAlong();
+                },
+                error: function () {
+                    // if it's not last one Move Along
+                    if (Shopify.queue.length) {
+                        Shopify.moveAlong()
+                    }
+                }
+            });
+        } else {
+            //redirect
+            $(".quick-checkout button").attr("disabled", false);
+            window.location.href="/cart";
+        }
+    };
+    Shopify.moveAlong();
+};
+
 });
